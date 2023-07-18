168ec6f1172bb0763af62afeac862736ae8483dd
Nils van der Werf <n.vanderw.92@gmail.com>
2023-07-18 22:51:24 +1000

    api call is working

--- src/components/GigList.js
+++ src/components/GigList.js
@@ -9,12 +9,25 @@ const GigList = () => {
     getGigs()
     .then((response) => {
       console.log('API Response:', response);
-      // setGigs(response.data); // Uncomment this line if you want to set the state with the response data
+      setGigs(response.data); // Uncomment this line if you want to set the state with the response data
     })
     .catch((error) => {
       console.error('Error fetching gigs:', error);
     });
   }, []);
+
+  return (
+    <div>
+      <h1>Gigs List</h1>
+      <ul>
+        {gigs.map(gig => (
+          <li key={gig.id}>
+            {gig.title} - {gig.date} - {gig.venue?.name} - {gig.genre?.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default GigList;
